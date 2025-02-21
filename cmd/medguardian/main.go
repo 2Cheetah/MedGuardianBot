02f696aa444be98ae873604894223c0a58c9e684
Denis Safiullin <fckninja@gmail.com>
2025-02-21 12:59:43 +0000

    adds main bot functionality and linting to Makefile

---
+++ cmd/medguardian/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// Send any text message to the bot after the bot has been started
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	apiToken := os.Getenv("API_TOKEN")
+	b, err := bot.New(apiToken, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+	if err != nil {
+		log.Fatalf("couldn't send message, err %v", err)
+	}
+}
