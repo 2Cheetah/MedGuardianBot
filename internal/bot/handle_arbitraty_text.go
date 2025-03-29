279bdaafca1e376c0a1e4906a8c8af9871eec768
Denis Safiullin <fckninja@gmail.com>
2025-03-29 06:56:28 +0000

    Refactor handleArbitraryText and add HandleDialog method in DialogService; improve dialog management and streamline notification creation process

--- internal/bot/handle_arbitraty_text.go
+++ internal/bot/handle_arbitraty_text.go
@@ -2,9 +2,7 @@ package bot
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
-	"time"
 
 	"github.com/2Cheetah/MedGuardianBot/internal/domain"
 	"github.com/go-telegram/bot"
@@ -12,40 +10,16 @@ import (
 )
 
 func (tb *TelegramBot) handleArbitraryText(ctx context.Context, b *bot.Bot, update *models.Update) {
-	// Check dialogs states, if any is in STARTED state
 	userID := update.Message.From.ID
-	slog.Info("checking if user has STARTED dialogs", "userID", userID)
-	dialog, err := tb.DialogService.GetActiveDialogByUserId(userID)
+	context := update.Message.Text
+	dialog := &domain.Dialog{
+		UserID:  userID,
+		Context: context,
+	}
+	msg, err := tb.DialogService.HandleDialog(dialog)
 	if err != nil {
-		slog.Error("couldn't GetActiveDialogByUserId", "error", err)
+		slog.Error("couldn't handleArbitraryText", "error", err)
 		return
 	}
-	slog.Info("received from GetActiveDialogByUserId", "dialog", dialog)
-	if dialog != nil {
-		switch dialog.Command {
-		case "create_notification":
-			if dialog.Context == "" {
-				dialog.Context = "schedule: " + update.Message.Text
-				dialog.UpdatedAt = time.Now().UTC()
-				if err := tb.DialogService.UpdateActiveDialog(dialog); err != nil {
-					slog.Error("couldn't UpdateActiveDialog from handle_arbitraty_text.go", "error", err)
-				}
-				msg := "What do you want me notify you about? What is the notificaiton text?"
-				sendMsg(ctx, b, update.Message.Chat.ID, msg)
-			} else {
-				dialog.Context += " text: " + update.Message.Text
-				dialog.UpdatedAt = time.Now().UTC()
-				dialog.State = domain.DialogStatusFinished
-				if err := tb.DialogService.UpdateActiveDialog(dialog); err != nil {
-					slog.Error("couldn't UpdateActiveDialog from handle_arbitraty_text.go", "error", err)
-				}
-				msg := fmt.Sprintf("Success! Notification created! %s", dialog.Context)
-				sendMsg(ctx, b, update.Message.Chat.ID, msg)
-			}
-			return
-		}
-	}
-	// If no dialogs in STARTED state, respond with help message
-	msg := "No active dialogs found. Select a command."
 	sendMsg(ctx, b, update.Message.Chat.ID, msg)
 }
--- internal/service/dialog_service.go
+++ internal/service/dialog_service.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/2Cheetah/MedGuardianBot/internal/domain"
 )
@@ -31,4 +33,45 @@ func (ds *DialogService) GetActiveDialogByUserId(userID int64) (*domain.Dialog, 
 
 func (ds *DialogService) UpdateActiveDialog(dialog *domain.Dialog) error {
 	return ds.repo.UpdateActiveDialog(dialog)
+}
+
+// ArbitraryDialog handles dialogs before completing flow for commands
+func (ds *DialogService) HandleDialog(d *domain.Dialog) (string, error) {
+	// Check if any dialog for the user is in STARTED state
+	slog.Info("checking if user has STARTED dialogs", "userID", d.UserID)
+	dDB, err := ds.repo.GetActiveDialogByUserId(d.UserID)
+	if err != nil {
+		slog.Error("couldn't GetActiveDialogByUserId", "error", err)
+		return "", fmt.Errorf("couldn't GetActiveDialogByUserId, error: %w", err)
+	}
+	slog.Info("received from GetActiveDialogByUserId", "dialog", dDB)
+	if dDB != nil {
+		switch dDB.Command {
+		case "create_notification":
+			if dDB.Context == "" {
+				dDB.Context = "schedule: " + d.Context
+				dDB.UpdatedAt = time.Now().UTC()
+				if err := ds.repo.UpdateActiveDialog(dDB); err != nil {
+					slog.Error("couldn't UpdateActiveDialog from handle_arbitraty_text.go", "error", err)
+				}
+				msg := "What do you want me notify you about? What is the notificaiton text?"
+				return msg, nil
+			} else {
+				dDB.Context += " text: " + d.Context
+				dDB.UpdatedAt = time.Now().UTC()
+				dDB.State = domain.DialogStatusFinished
+				if err := ds.repo.UpdateActiveDialog(dDB); err != nil {
+					slog.Error("couldn't UpdateActiveDialog from handle_arbitraty_text.go", "error", err)
+				}
+				msg := fmt.Sprintf("Success! Notification created! %s", dDB.Context)
+				return msg, nil
+			}
+		default:
+			msg := fmt.Sprintf("Found STARTED dialog for command %s", dDB.Command)
+			return msg, nil
+		}
+	}
+	// If no dialogs in STARTED state, respond with help message
+	msg := "No active dialogs found. Select a command."
+	return msg, nil
 }
